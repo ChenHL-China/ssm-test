2b809bca2c5661d6fb128b31e8cc0afc3a7b32bb
陈宏雷 <853976293@qq.com>
2018-03-08 23:12:43 +0800

    Weather测试

---
+++ src/main/java/SSM/Pojo/Weather.java
@@ -0,0 +1,12 @@
+package SSM.Pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Weather {
+    private String Weather_Name;
+    private Integer Weather_Effect;
+    private String Weather_Notes;
+}
